378e68195c139b747dc1f1091f6ab70c067100f3
fanwenbo <listenecho@foxmail.com>
2020-06-11 09:25:26 +0800

    多包下载

--- src/command/install.js
+++ src/command/install.js
@@ -6,61 +6,67 @@ const fs = require("fs");
 const path = require('path')
 const ora = require('ora');
 const chalk = require('chalk');
-
 const spinner = ora()
-/** github地址 */
-const SROUCE_URL = 'listenecho/mercy-shop-server'
 
 /** 
  * 实现功能: 通过lake install xxx 实现 npm install 类似的下载，操作并将文件下载到package
  * 
  * 实现步骤： 
  *  1. 新建package文件夹
- *  2. 命令解析
+ *  2. 命令解析, 涉及多条指令 下载
  *  3. 将文件下载到package文件夹
  * 
 */
 progarm.command('install')
-.description('下载 github 项目')
-.action(options => {
-   
-    const { args = [] } = options
-    // 获取参数第一位
-    let currentPck = args[0]
-    if(!currentPck) return
-    isExitFile('package')
-       doExeca(currentPck)
-})
+   .description('下载 github 项目')
+   .action(options => {
+      const { args = [] } = options
+      // 获取参数第一位
+      if (!args.length) return;
+      isExitFile('package')
+      execaPck(args)
+   })
 
 
 /**
  * 判断文件夹是否存在
  */
 
- function isExitFile(fileName) {
-    if(fs.existsSync(fileName)) {
-        process.exit(1);
-    }
-    fs.mkdir(fileName, (err => err && console.log(err)))
-    // execa('npm init')
- }
- /**
-  * 下载包
-  * 
-  */
+function isExitFile(fileName) {
+   if (fs.existsSync(fileName)) {
+      return
+   }
+   fs.mkdir(fileName, (err => err && console.log(err)))
+}
+/**
+ * 下载包
+ * 
+ */
 
- async function doExeca(pck) {
-     try {
-        spinner.start(`正在下载${pck}`)
-        const result = await execa(`npm install ${pck} g --prefix ${path.resolve(__dirname,'../../package')} `)
-        spinner.stop()
-        
-     } catch (error) {
-        spinner.stop()
-     }
-   
-  }
+async function execaPck(packages) {
+   for (const pck of packages) {
+      await doExeca(pck)
+   }
+   console.log(chalk.green(`下载完毕.....`))
+}
 
+
+async function doExeca(pck) {
+   return new Promise(async (resolve, reject) => {
+      try {
+         spinner.start(chalk.blue(`正在下载${pck}`))
+         await execa(`npm install ${pck} -g --prefix ${path.resolve(__dirname, '../../package')} `)
+         spinner.stop()
+         console.log(chalk.red(`安装完成${pck}`))
+      } catch (error) {
+         reject(error)
+         spinner.stop()
+         console.log(chalk.yellow(`${pck}包下载异常`))
+      }
+      resolve()
+      console.log(chalk.yellow(`${pck}包下载完毕.....`))
+   })
+}
 module.exports = {
-    install: progarm
+   install: progarm
 }
